8de723793008e0adb79af6a0d71019f39d31bafb
G_Goncalves <83215603+G-GoncaIves@users.noreply.github.com>
2024-07-29 16:58:22 +0200

    Update models.py

--- utils/ml/models.py
+++ utils/ml/models.py
@@ -87,25 +87,25 @@ class Net3D(torch.nn.Module):
             torch.nn.Conv3d(
                 in_channels = 1,
                 out_channels = 6,
-                kernel_size = [5,5,3]
+                kernel_size = [5,5,5]
             ),
             torch.nn.Tanh(),
             torch.nn.MaxPool3d(
-                kernel_size = [2,2,1],
-                stride = [2, 2, 1]
-            ),
+                kernel_size = [2,2,2],
+                stride = 2
+            ),            
             torch.nn.Conv3d(
                 in_channels = 6,
                 out_channels = 12,
                 kernel_size = [3,3,3]
             ),
-            torch.nn.Tanh(),
+            torch.nn.Tanh(),            
             torch.nn.Conv3d(
                 in_channels = 12,
                 out_channels = 12,
                 kernel_size = [3,3,3]
             ),
-            torch.nn.Tanh(),
+            torch.nn.Tanh(),            
             torch.nn.Conv3d(
                 in_channels = 12,
                 out_channels = 12,
@@ -113,31 +113,26 @@ class Net3D(torch.nn.Module):
             ),
             torch.nn.Tanh(),
             torch.nn.MaxPool3d(
-                kernel_size = [2,2,1],
-                stride = [2, 2, 1]
+                kernel_size = [2,2,2],
+                stride = 2
             ),
             torch.nn.Conv3d(
                 in_channels = 12,
                 out_channels = 24,
-                kernel_size = [3,3,2]
+                kernel_size = [3,3,3]
             ),
             torch.nn.Tanh(),
             torch.nn.Conv3d(
                 in_channels = 24,
                 out_channels = 24,
-                kernel_size = [3,3,2]
-            ),
-            torch.nn.Tanh(),        
-            torch.nn.MaxPool3d(
-                kernel_size = [2,2,1],
-                stride = [2, 2, 1]
-            ),
-            torch.nn.Flatten(),
-            torch.nn.Linear(
-                in_features = 720,
-                out_features = 24
+                kernel_size = [3,3,3]
             ),
             torch.nn.Tanh(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),
+            torch.nn.Flatten(),
             torch.nn.Linear(
                 in_features = 24,
                 out_features = 12
@@ -145,15 +140,19 @@ class Net3D(torch.nn.Module):
             torch.nn.Tanh(),
             torch.nn.Linear(
                 in_features = 12,
+                out_features = 6
+            ),
+            torch.nn.Tanh(),
+            torch.nn.Linear(
+                in_features = 6,
                 out_features = out_dim
-            )       
+            )      
         )
         self.net.apply(self.weights_init)
     
     def weights_init(self, m):
         if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
             torch.nn.init.xavier_uniform_(m.weight)
-    
+            
     def forward(self, x):
         return self.net(x)
-    
