5bc6a244d21101a27b46df4fb9fb4794e0eae752
G_Goncalves <83215603+G-GoncaIves@users.noreply.github.com>
2024-08-28 12:51:18 +0200

    Update models.py

--- utils/ml/models.py
+++ utils/ml/models.py
@@ -156,3 +156,222 @@ class Net3D(torch.nn.Module):
             
     def forward(self, x):
         return self.net(x)
+
+class Net3D_v1(torch.nn.Module):
+    
+    def __init__(self, out_dim):
+        super(Net3D_v1, self).__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Conv3d(
+                in_channels = 1,
+                out_channels = 6,
+                kernel_size = [5,5,5]
+            ),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),            
+            torch.nn.Conv3d(
+                in_channels = 6,
+                out_channels = 12,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.ReLU(),            
+            torch.nn.Conv3d(
+                in_channels = 12,
+                out_channels = 12,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.ReLU(),            
+            torch.nn.Conv3d(
+                in_channels = 12,
+                out_channels = 12,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),
+            torch.nn.Conv3d(
+                in_channels = 12,
+                out_channels = 24,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.ReLU(),
+            torch.nn.Conv3d(
+                in_channels = 24,
+                out_channels = 24,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),
+            torch.nn.Flatten(),
+            torch.nn.ReLU,
+            torch.nn.Linear(
+                in_features = 24,
+                out_features = 12
+            ),
+            torch.nn.ReLU(),
+            torch.nn.Linear(
+                in_features = 12,
+                out_features = 6
+            ),
+            torch.nn.ReLU(),
+            torch.nn.Linear(
+                in_features = 6,
+                out_features = out_dim
+            )      
+        )
+        self.net.apply(self.weights_init)
+    
+    def weights_init(self, m):
+        if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight)
+            
+    def forward(self, x):
+        return self.net(x)
+
+class Net3D_v2(torch.nn.Module):
+    
+    def __init__(self, out_dim):
+        super(Net3D_v2, self).__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Conv3d(
+                in_channels = 1,
+                out_channels = 8,
+                kernel_size = [6,5,5]
+            ),
+            torch.nn.Tanh(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),            
+            torch.nn.Conv3d(
+                in_channels = 8,
+                out_channels = 16,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),            
+            torch.nn.Conv3d(
+                in_channels = 16,
+                out_channels = 32,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),            
+            torch.nn.Conv3d(
+                in_channels = 32,
+                out_channels = 64,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),
+            torch.nn.Conv3d(
+                in_channels = 64,
+                out_channels = 128,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),
+            torch.nn.Conv3d(
+                in_channels = 128,
+                out_channels = 128,
+                kernel_size = [1,3,3]
+            ),
+            torch.nn.Tanh(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),
+            torch.nn.Flatten(),
+            torch.nn.Linear(
+                in_features = 128*1*2*2,
+                out_features = 256
+            ),
+            torch.nn.Tanh(),
+            torch.nn.Linear(
+                in_features = 256,
+                out_features = 128
+            ),
+            torch.nn.Tanh(),
+            torch.nn.Linear(
+                in_features = 128,
+                out_features = out_dim
+            )      
+        )
+        self.net.apply(self.weights_init)
+    
+    def weights_init(self, m):
+        if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight)
+            
+    def forward(self, x):
+        return self.net(x)
+
+class Net3D_v3(nn.Module):
+    
+    def __init__(self, out_dim):
+        super(Net3D_v3, self).__init__()
+        self.net = nn.Sequential(
+            nn.Conv3d(
+                in_channels=1,
+                out_channels=8,
+                kernel_size=[6, 5, 5]
+            ),
+            nn.ReLU(),
+            nn.MaxPool3d(
+                kernel_size=[2, 2, 2],
+                stride=2
+            ),            
+            nn.Conv3d(
+                in_channels=8,
+                out_channels=16,
+                kernel_size=[3, 3, 3]
+            ),
+            nn.ReLU(),            
+            nn.Conv3d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=[3, 3, 3]
+            ),
+            nn.ReLU(),
+            nn.MaxPool3d(
+                kernel_size=[2, 2, 2],
+                stride=2
+            ),
+            nn.Conv3d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=[3, 3, 3]
+            ),
+            nn.ReLU(),
+            nn.MaxPool3d(
+                kernel_size=[2, 2, 2],
+                stride=2
+            ),
+            nn.Flatten(),
+            nn.Linear(
+                in_features=64 * 1 * 2 * 2,
+                out_features=128
+            ),
+            nn.ReLU(),
+            nn.Linear(
+                in_features=128,
+                out_features=64
+            ),
+            nn.ReLU(),
+            nn.Linear(
+                in_features=64,
+                out_features=out_dim
+            )      
+        )
+    
+    def forward(self, x):
+        return self.net(x)
+
