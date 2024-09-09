655424d3cebb66c927e5c0279965da5a58f98786
G_Goncalves <83215603+G-GoncaIves@users.noreply.github.com>
2024-09-09 23:24:08 +0200

    Update models.py

--- utils/ml/models.py
+++ utils/ml/models.py
@@ -228,6 +228,79 @@ class Net3D_Positive(torch.nn.Module):
             torch.nn.ReLU()      
         )
         self.net.apply(self.weights_init)
+
+class Net3D_Positive_v2(torch.nn.Module):
+    
+    def __init__(self, out_dim):
+        super(Net3D_Positive, self).__init__()
+        self.net = torch.nn.Sequential(
+            torch.nn.Conv3d(
+                in_channels = 1,
+                out_channels = 6,
+                kernel_size = [5,5,5]
+            ),
+            torch.nn.Tanh(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),            
+            torch.nn.Conv3d(
+                in_channels = 6,
+                out_channels = 12,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),            
+            torch.nn.Conv3d(
+                in_channels = 12,
+                out_channels = 12,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),            
+            torch.nn.Conv3d(
+                in_channels = 12,
+                out_channels = 12,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),
+            torch.nn.Conv3d(
+                in_channels = 12,
+                out_channels = 24,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),
+            torch.nn.Conv3d(
+                in_channels = 24,
+                out_channels = 24,
+                kernel_size = [3,3,3]
+            ),
+            torch.nn.Tanh(),
+            torch.nn.MaxPool3d(
+                kernel_size = [2,2,2],
+                stride = 2
+            ),
+            torch.nn.Flatten(),
+            torch.nn.Linear(
+                in_features = 24,
+                out_features = 12
+            ),
+            torch.nn.Tanh(),
+            torch.nn.Linear(
+                in_features = 12,
+                out_features = 6
+            ),
+            torch.nn.Tanh(),
+            torch.nn.Linear(
+                in_features = 6,
+                out_features = out_dim
+            ),
+            torch.nn.ReLU()      
+        )
+        self.net.apply(self.weights_init)
+
     
     def weights_init(self, m):
         if isinstance(m, torch.nn.Conv2d) or isinstance(m, torch.nn.Linear):
