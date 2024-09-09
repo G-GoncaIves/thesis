3272db8cb439a1da5d5ef23889679b892b6ee9a0
G-Goncalves <goncalo.mota.goncalves@gmail.com>
2024-09-09 21:22:23 +0000

    remote changes

--- scripts/train_models.py
+++ scripts/train_models.py
@@ -4,7 +4,7 @@ import sys
 import torch
 
 sys.path.insert(1, '/home/goncalo/Projects/thesis/utils/ml')
-from models import Net3D, Net2D, Net3D_v1, Net3D_v2, Net3D_v3
+from models import Net3D_Positive, Net3D
 from train import run_multiple_trains
 
 default_config = {
@@ -12,13 +12,14 @@ default_config = {
     "lr" : 1e-4,
     "data_size" : 10000,
     "batch_size" : 100,
-    "rescale" : 1,
+    "rescale_labels" : 1,
     "loss_fn" : torch.nn.MSELoss(),
     "patience" : 300,
     "min_delta" : 0,
     "model" : None,
     "no_td" : None,
     "param" : None,
+    "normalize_data" : False,
     "data_path" : "/home/goncalo/Projects/thesis/scripts/Data/noisy_train/m_2/Videos",
     "generation_df_path" : "/home/goncalo/Projects/thesis/scripts/Data/noisy_train/m_2/noisy_train_simulated.pickle"
 }
@@ -26,12 +27,23 @@ default_config = {
 configs_list = [
    {
         "param" : ["im_td"],
-        "model" : Net3D_v2(out_dim=1),
+        "model" : Net3D_Positive(out_dim=1),
         "epochs" : 1000,
         "data_size" : 15000,
         "no_td" : False,
         "rescale" : 1/200,
-        "patience" : 50
+        "patience" : 100,
+	"normalize_data" : True
+    },
+    {
+        "param" : ["im_td"],
+        "model" : Net3D(out_dim=1),
+        "epochs" : 1000,
+        "data_size" : 15000,
+        "no_td" : False,
+        "rescale" : 1/200,
+        "patience" : 100,
+        "normalize_data" : True
     }
 ]
 
